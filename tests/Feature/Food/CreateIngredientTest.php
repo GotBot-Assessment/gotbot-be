24a1770f84e29ad2fb657e0ae784a01b8f6e2f00
Innocent Mazando <innocentmazando@gmail.com>
2024-08-14 20:40:57 +0200

    Tested creating an ingredient

--- tests/Feature/Food/CreateIngredientTest.php
+++ tests/Feature/Food/CreateIngredientTest.php
@@ -1,7 +1,42 @@
 <?php
 
-test('example', function () {
-    $response = $this->get('/');
+use App\Models\Food;
+use App\Models\User;
 
-    $response->assertStatus(200);
+test('it blocks an unauthenticated call', function () {
+    $response = $this->postJson('/api/foods/1/ingredients', []);
+
+    $response->assertUnauthorized();
+});
+
+test('it throws an error if a food item isnt found', function () {
+    $user = User::factory()->create();
+    $this->actingAs($user, 'api');
+
+    $response = $this->postJson('/api/foods/1/ingredients', []);
+    $response->assertNotFound();
+});
+
+test('it throws an error if a name is not supplied', function () {
+    Food::factory()->create();
+    $user = User::factory()->create();
+    $this->actingAs($user, 'api');
+
+    $response = $this->postJson('/api/foods/1/ingredients', []);
+    $response->assertUnprocessable();
+    $response->assertJsonValidationErrors([
+        'name' => 'The name field is required.'
+    ]);
+});
+
+test('it creates a new ingredient', function () {
+    Food::factory()->create();
+    $user = User::factory()->create();
+    $this->actingAs($user, 'api');
+    $response = $this->postJson('/api/foods/1/ingredients', [
+        'name' => 'Test',
+    ]);
+
+    $response->assertCreated();
+    $response->assertJsonStructure(['name']);
 });
