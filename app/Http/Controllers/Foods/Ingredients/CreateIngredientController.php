fff851e8ace553a4c9a236c3f5ee20a0ef55d180
Innocent Mazando <innocentmazando@gmail.com>
2024-08-14 18:40:05 +0200

    Wired controller to create ingredi

--- app/Http/Controllers/Foods/Ingredients/CreateIngredientController.php
+++ app/Http/Controllers/Foods/Ingredients/CreateIngredientController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers\Foods\Ingredients;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Food\SaveIngredientRequest;
+use App\Http\Resources\IngredientReource;
+use App\Models\Food;
 use Illuminate\Http\Request;
 
 class CreateIngredientController extends Controller
@@ -10,8 +13,10 @@ class CreateIngredientController extends Controller
     /**
      * Handle the incoming request.
      */
-    public function __invoke(Request $request)
-    {
-        //
+    public function __invoke(Food $food, SaveIngredientRequest $request) {
+        $ingredient = $food->ingredients()
+            ->create($request->validated());
+
+        return new IngredientReource($ingredient);
     }
 }
