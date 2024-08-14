cba5b4d182229d6e4c0333a9a03044e33de0ec63
Innocent Mazando <innocentmazando@gmail.com>
2024-08-14 17:22:59 +0200

    wired controller to create user

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -2,15 +2,24 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
 
 class RegistrationController extends Controller
 {
     /**
      * Handle the incoming request.
      */
-    public function __invoke(Request $request)
-    {
-        //
+    public function __invoke(RegisterRequest $request) {
+        $userData = $request->validated();
+        $userData['password'] = bcrypt($userData['password']);
+
+        $user = User::create($userData);
+        $token = $user->createToken('user-token')->plainTextToken;
+
+        return response()->json([
+            'status' => 'success',
+            'token'  => $token
+        ]);
     }
 }
