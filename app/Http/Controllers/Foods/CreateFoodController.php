92d73e8ff0d748fcee86d6986191dd196007492f
Innocent Mazando <innocentmazando@gmail.com>
2024-08-14 18:01:15 +0200

    Linked resource to create food controller

--- app/Http/Controllers/Foods/CreateFoodController.php
+++ app/Http/Controllers/Foods/CreateFoodController.php
@@ -3,15 +3,18 @@
 namespace App\Http\Controllers\Foods;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\Food\SaveFoodRequest;
+use App\Http\Resources\FoodResource;
+use App\Models\Food;
 
 class CreateFoodController extends Controller
 {
     /**
      * Handle the incoming request.
      */
-    public function __invoke(Request $request)
-    {
-        //
+    public function __invoke(SaveFoodRequest $request) {
+        $food = Food::create($request->validated());
+
+        return new FoodResource($food);
     }
 }
