3307834fbda7cb616ff3bfedca2a62ba0bb4ac38
Innocent Mazando <innocentmazando@gmail.com>
2024-08-14 18:46:13 +0200

    Updating ingredient

--- app/Http/Controllers/Foods/Ingredients/UpdateIngredientController.php
+++ app/Http/Controllers/Foods/Ingredients/UpdateIngredientController.php
@@ -3,15 +3,24 @@
 namespace App\Http\Controllers\Foods\Ingredients;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Food\SaveIngredientRequest;
+use App\Models\Food;
+use App\Models\Ingredient;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class UpdateIngredientController extends Controller
 {
     /**
      * Handle the incoming request.
      */
-    public function __invoke(Request $request)
-    {
-        //
+    public function __invoke(Food $food, Ingredient $ingredient, SaveIngredientRequest $request) {
+        if ($ingredient->foodId !== $food->id) {
+            abort(Response::HTTP_FORBIDDEN, 'You can not update an ingredient that doesnt belong to this meal.');
+        }
+
+        $ingredient->update($request->validated());
+
+        return $ingredient->fresh();
     }
 }
