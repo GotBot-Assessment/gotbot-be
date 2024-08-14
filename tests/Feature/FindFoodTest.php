9b93f165c9b38584dc55074ae228836c8bd6fed7
Innocent Mazando <innocentmazando@gmail.com>
2024-08-14 20:31:57 +0200

    Tested viewing item

---
+++ tests/Feature/FindFoodTest.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Models\Food;
+use App\Models\User;
+
+test('it blocks an unauthenticated call', function () {
+    $response = $this->getJson('/api/foods/1');
+
+    $response->assertUnauthorized();
+});
+
+test('throws 404 if food item is not found', function () {
+    $user = User::factory()->create();
+    $this->actingAs($user, 'api');
+
+    $response = $this->getJson('/api/foods/1');
+
+    $response->assertNotFound();
+});
+
+test('it returns a single food item', function () {
+    Food::factory()->create();
+    $user = User::factory()->create();
+    $this->actingAs($user, 'api');
+
+    $response = $this->getJson('/api/foods/1');
+
+    $response->assertOk();
+    $response->assertJsonStructure([
+        'id',
+        'name',
+        'description',
+        'type',
+        'price',
+        'createdAt',
+        'updatedAt',
+        'ingredients',
+    ]);
+});
