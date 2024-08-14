31879a1b3026a8c486f7d918fddc06a73d952e96
Innocent Mazando <innocentmazando@gmail.com>
2024-08-14 17:49:56 +0200

    wipping food controllers

---
+++ app/Http/Controllers/Foods/Ingredients/UpdateIngredientControoller.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Foods\Ingredients;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class UpdateIngredientControoller extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        //
+    }
+}
