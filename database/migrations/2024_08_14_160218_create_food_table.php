db9fcafba01c92c012908a177b2c553c0d5cfaad
Innocent Mazando <innocentmazando@gmail.com>
2024-08-14 17:13:35 +0200

    Seeded ingredients

--- database/migrations/2024_08_14_160218_create_food_table.php
+++ database/migrations/2024_08_14_160218_create_food_table.php
@@ -9,7 +9,7 @@ return new class extends Migration {
      * Run the migrations.
      */
     public function up(): void {
-        Schema::create('food', function (Blueprint $table) {
+        Schema::create('foods', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->text('description');
@@ -23,6 +23,6 @@ return new class extends Migration {
      * Reverse the migrations.
      */
     public function down(): void {
-        Schema::dropIfExists('food');
+        Schema::dropIfExists('foods');
     }
 };
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,9 @@
 namespace Database\Seeders;
 
 use App\Models\Food;
+use App\Models\Ingredient;
 use App\Models\User;
+
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,15 +14,19 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
-    public function run(): void
-    {
+    public function run(): void {
         User::factory(10)->create();
 
-        Food::factory(50)->create();
+        Food::factory(50)->create()
+            ->each(function ($food) {
+                $food->ingredients()->saveMany(
+                    Ingredient::factory(rand(1, 5))->make()
+                );
+            });
 
-/*        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);*/
+        /*        User::factory()->create([
+                    'name' => 'Test User',
+                    'email' => 'test@example.com',
+                ]);*/
     }
 }
