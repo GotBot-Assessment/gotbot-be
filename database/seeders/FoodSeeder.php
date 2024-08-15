cabfd59be6ffca5f1f822a9a1d3778452ee5862a
Innocent Mazando <innocentmazando@gmail.com>
2024-08-15 15:02:38 +0200

    Optmized imports

--- database/seeders/FoodSeeder.php
+++ database/seeders/FoodSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\Food;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Http;
