834b1b7f27a9d892248864fdb8021402f5648d87
Innocent Mazando <innocentmazando@gmail.com>
2024-08-16 19:41:50 +0200

    Tested blocking logout

--- tests/Feature/Authentication/LogoutTest.php
+++ tests/Feature/Authentication/LogoutTest.php
@@ -1,7 +1,19 @@
 <?php
 
+use App\Models\User;
+
 test('it blocks an unauthenticated call', function () {
     $response = $this->getJson('/api/auth/logout', []);
 
     $response->assertUnauthorized();
+});
+
+test('it logs the user out', function () {
+    $user = User::factory()->create();
+    $this->actingAs($user, 'api');
+
+    $this->getJson('/api/auth/logout', [])
+        ->assertSuccessful();
+    $this->assertGuest();
+    $this->assertNull(auth()->user());
 });
